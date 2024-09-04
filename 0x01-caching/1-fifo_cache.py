0a8faffa0d8bc0d8f1c63fd51364ca42081563df
Lain0x0 <lainyamato03@gmail.com>
2024-09-04 16:08:03 +0000

    0x1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 1-FIFO_caching """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ A class FIFO """
+    def __init__(self):
+        """ __Constructor__ """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item cache """
+        if key is None or item is None:
+            return
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returning the value of key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
