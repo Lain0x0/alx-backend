6402a3450f9d126aab6a8cb3c2b8745ec39e84df
Lain0x0 <lainyamato03@gmail.com>
2024-09-10 18:47:03 +0000

    pybabel-config

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Configuring template using `Babel`"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Set up Config class for LANGUAGES & 
+    others parmaters"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Returning accept_language paramater for best match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Rendering (`3-index.html`) template"""
+    return render_template("3-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
