41fd1504e52a704d03d117b6fc26d7e925d38a3a
Lain0x0 <lainyamato03@gmail.com>
2024-09-10 18:04:48 +0000

    flask-basic

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-""" Using flask basics configurations """
-from flask import Flask ,render_template
+""" Basic flask set up """
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index_page():
-    """ Rendering index.html page """
+def index():
+    """ Rendering the 'index.html' template """
     return render_template("0-index.html")
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
