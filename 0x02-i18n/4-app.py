8f270833c9a53dff51a5e12740f7cfdc882f200e
Lain0x0 <lainyamato03@gmail.com>
2024-09-10 20:49:10 +0000

    0x4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Configure Web App Using Flask & Flask-Babel """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Set Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Returning accept_languages parameter from request """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ Indexing and handling routes """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
